0682352a6c32ac8cf3840d8e47014b4d0a9d580d
Chinzorig Otgonjargal <chinzorig.o@gmail.com>
2022-10-04 20:57:24 +0800

    controller

--- models/ProductModel.js/Productmodel.js
+++ models/ProductModel.js/Productmodel.js
@@ -26,10 +26,40 @@ export const getProductById = (id, result) => {
 };
 
 // Insert product to database
-
-
-// Delete Product to Database
-
-export const deleteProductById = (id, result) => {
-    db.query("DELETE FROM product WHERE product_id = ?", [id], (err, result))
+export const insertProduct = (data, result) => {
+    db.query("INSERT INTO product SET?"), [data], (err, results) => {
+        if (err) {
+            console.log(err);
+            result(err, null);
+          } else {
+            result(null, results);
+        }
+    }
 }
+
+// Update product to database
+export const updateProductById = (data, id, result) => {
+    db.query("UPDATE product SET product_name = ?, product_price = ? WHERE product_id = ?"), [data.product_name, data.product_price, id], (err, results) => {
+        if (err) {
+            console.log(err);
+            result(err, null);
+          } else {
+            result(null, results);
+        }
+    }
+}
+
+// Delete product from database
+export const deleteProductById = (id, result) => {
+    db.query("DELETE from product WHERE product_id"), [id], (err, results) => {
+        if (err) {
+            console.log(err);
+            result(err, null);
+          } else {
+            result(null, results);
+        }
+    }
+}
+
+
+
